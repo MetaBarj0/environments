7cbf3348fb5e39aca3cf24883d984b12255ef18e
MetaBarj0 <troctsch.cpp@gmail.com>
2024-09-17 14:56:24 +0000

    common(neovim): easy align

--- common/neovim/USER_HOME_DIR/.config/nvim/init.lua
+++ common/neovim/USER_HOME_DIR/.config/nvim/init.lua
@@ -1,11 +1,5 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- editor config
 require("config.editor")
-
--- lsp config
 require("config.lsp")
-
--- easy motion config
 require("config.easymotion")
+require("config.easyalign")
