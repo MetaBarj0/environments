f2acd5273ea28d146ec6f58658926fb0d14164c4
sebastien.levy <sebastien.levy@ringover.com>
2024-09-30 15:30:18 +0200

    common(neovim): dap
    no need to specify an absolute path anymore

--- common/neovim/USER_HOME_DIR/.config/nvim/lua/config/dap.lua
+++ common/neovim/USER_HOME_DIR/.config/nvim/lua/config/dap.lua
@@ -2,7 +2,7 @@ local dap = require("dap")
 
 dap.adapters.lldb = {
 	type = "executable",
-	command = "/usr/bin/lldb-dap", -- adjust as needed, must be absolute path
+	command = "lldb-dap",
 	name = "lldb",
 }
 
